d490504b9675f8cba5c891716792de314347a0ed
aimedidierm <aimedidiermugisha@gmail.com>
2021-04-14 05:31:02 +0200

    init homepage

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+if (isset($_GET['generated']) && $_GET['generated'] == "false") {
+    unset($_GET['generated']);
+    echo '<script>alert("Timetable not generated yet!!");</script>';
+}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>School timetable</title>
+    <!-- BOOTSTRAP CORE STYLE CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet"/>
+    <!-- FONT AWESOME CSS -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet"/>
+    <!-- FLEXSLIDER CSS -->
+    <link href="assets/css/flexslider.css" rel="stylesheet"/>
+    <!-- CUSTOM STYLE CSS -->
+    <link href="assets/css/style.css" rel="stylesheet"/>
+    <!-- Google	Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'/>
+    <!-- ICON -->
+    <link rel="shortcut icon" href="img/logo.png">
+
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top " id="menu">
+    <div class="container">
+        <div align="center">
+            <h3 align="center">SCHOOL TIME TABLE</h3>
+        </div>
+    </div>
+</div>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators" style="margin-bottom: 160px">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="img/slide1.png" alt="Slide 1">
+        </div>
+
+        <div class="item">
+            <img src="img/slide2.png" alt="Slide 2">
+        </div>
+
+        <div class="item">
+            <img src="img/slide3.png" alt="Slide 3">
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    function genpdf() {
+        var doc = new jsPDF();
+
+        doc.addHTML(document.getElementById('TT'), function () {
+            doc.save('demo timetable.pdf');
+        });
+        window.alert("Downloaded!");
+    }
+</script>
+<div align="center" STYLE="margin-top: 30px">
+    <button data-scroll-reveal="enter from the bottom after 0.2s"
+            id="teacherlogin" class="btn btn-info btn-lg">TEACHER LOGIN
+    </button>
+    <button data-scroll-reveal="enter from the bottom after 0.2s"
+            id="adminlogin" class="btn btn-success btn-lg">ADMIN LOGIN
+    </button>
+</div>
+<br>
+<div align="center">
+    <form data-scroll-reveal="enter from the bottom after 0.2s" action="#" method="post">
+        <select id="select_semester" name="select_semester" class="list-group-item">
+            <option selected disabled>Select your class</option>
+            <option value="3"> Electronics and telecommunication technology Lever 2</option>
+        </select>
+        <button type="submit" class="btn btn-info btn-lg" style="margin-top: 10px">Download</button>
+    </form>
+</div>
+
+</body>
+</html>
